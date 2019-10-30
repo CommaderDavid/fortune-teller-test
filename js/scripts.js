673137c6293232a88020cd62a6bc0b5e64879ba3
David Boedigheimer <stealth259@gmail.com>
2019-10-30 10:53:18 -0700

    Add if statement to backend, added show function to front end.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,31 @@
 // Back end
-
+var tellerResult = function(bad, good) {
+  if (bad === 6 && good === 6) {
+    return "A long, exciting, and crazy styled Life awaits you!"
+  } else if (bad >= 3 && good <= 2) {
+    return "Seek guidence from those you respect. For your life will be hard going for a time."
+  } else if (good >= 3 && bad <= 2) {
+    return "Your life may seem to have some downs, but it is about to get much better!"
+  } else {
+    return "Your fortune remains a mystery even to me."
+  }
+}
 // Front end
 $(document).ready(function() {
   $("form#teller-test").submit(function(e) {
     e.preventDefault();
-
+    var unluckyVote = 0;
+    var luckyVote = 0;
     $("#teller-show").show();
-    
+    $("input:checkbox[name=unlucky]:checked").each(function() {
+      var unluckyVote = parseInt($(this).val());
+    });
+    $("input:checkbox[name=lucky]:checked").each(function() {
+      var luckyVote = parseInt($(this).val());
+    });
+    var result = tellerResult(unluckyVote, luckyVote);
+    console.log(result);
+    $("#teller-show").append(result);
+    $("#teller-test").hide();
   });
 });
